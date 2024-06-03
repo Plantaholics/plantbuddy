44c6c46f833a80d176c988b94899bdfc7fdc7621
Sergio <sadiazcaballero@gmail.com>
2024-06-03 21:04:37 +0200

    care schema updated to solve problem in the frontend

--- models/Care.model.js
+++ models/Care.model.js
@@ -1,34 +1,28 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const CareSchema = new Schema (
-    [{
-        water: {
-            type: String,
-            enum: ["once a day", "once a week", "twice a week", "once every two weeks", "once a month"]
-        },
-        fertilization: {
-            type: String,
-            enum: ["every month", "every 3 months", "every 6 months"]
-        },
-        benefits: {
-            type: String
-        },
-        sunlight: {
-            type: String,
-            enum: ["morning", "midday", "afternoon", "all day"]
-        },
-        preferred_area: {
-            type: String,
-            enum: ["only indoor", "only outdoor", "indoor/outdoor", "humid places", "dry places"]
-        },
-        plant: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Plant"
-          }
-        }]
-    )
-
+const CareSchema = new Schema({
+  plant: { type: mongoose.Schema.Types.ObjectId, ref: "Plant" },
+  water: {
+    type: String,
+    enum: ["once a day", "once a week", "twice a week", "once every two weeks", "once a month"]
+  },
+  fertilization: {
+    type: String,
+    enum: ["every month", "every 3 months", "every 6 months"]
+  },
+  benefits: {
+    type: String
+  },
+  sunlight: {
+    type: String,
+    enum: ["morning", "midday", "afternoon", "all day"]
+  },
+  preferred_area: {
+    type: String,
+    enum: ["only indoor", "only outdoor", "indoor/outdoor", "humid places", "dry places"]
+  }
+});
 
 const Care = model("Care", CareSchema);
-module.exports = Care
+module.exports = Care;
